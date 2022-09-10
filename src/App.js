e0c7208dd48b24927831d2090f778b560ad34b9c
Kachisa D.N <kachdekan@gmail.com>
2022-09-10 12:42:01 +0300

    remove. increase, decrease and get the total

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { calculateTotals } from './features/cart/cartSlice'
+import { useSelector, useDispatch }  from 'react-redux'
 
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
   return (
   <main>
     <Navbar />
